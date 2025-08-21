b3dfe02c915097215e8631193d6d0c7bf980ff38
Gabriel Farro <farrogabriel90@gmail.com>
2025-08-21 18:23:42 -0500

    Proyecto base

---
+++ EjemploGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjemploGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
